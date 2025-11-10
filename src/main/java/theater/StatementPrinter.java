02acea99439dc3ba5364e86437569d961bf67ffd
celinec41 <celinesy.chen@mail.utoronto.ca>
2025-11-10 18:41:15 -0500

    Task 2.3

--- src/main/java/theater/StatementPrinter.java
+++ src/main/java/theater/StatementPrinter.java
@@ -33,8 +33,6 @@ public final class StatementPrinter {
                 "Statement for " + invoice.getCustomer() + System.lineSeparator()
         );
 
-        final NumberFormat frmt = NumberFormat.getCurrencyInstance(Locale.US);
-
         for (final Performance performance : invoice.getPerformances()) {
 
             // add volume credits
@@ -44,17 +42,21 @@ public final class StatementPrinter {
             result.append(String.format(
                     "  %s: %s (%s seats)%n",
                     getPlay(performance).getName(),
-                    frmt.format(getAmount(performance) / Constants.PERCENT_FACTOR),
+                    NumberFormat.getCurrencyInstance(Locale.US).format(getAmount(performance) / Constants.PERCENT_FACTOR),
                     performance.getAudience()));
             totalAmount += getAmount(performance);
         }
         result.append(String.format(
                 "Amount owed is %s%n",
-                frmt.format(totalAmount / Constants.PERCENT_FACTOR)));
+                usd(totalAmount)));
         result.append(String.format(
                 "You earned %s credits%n",
                 volumeCredits));
         return result.toString();
+    }
+
+    private static String usd(int totalAmount) {
+        return NumberFormat.getCurrencyInstance(Locale.US).format(totalAmount / Constants.PERCENT_FACTOR);
     }
 
     private int getVolumeCredits(Performance performance, int results) {
